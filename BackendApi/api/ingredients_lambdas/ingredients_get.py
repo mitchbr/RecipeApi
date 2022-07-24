3d38803ac532b56ea1e18dc694623c93cd871ea1
mitchbr <mitchell.dee.brown@gmail.com>
2022-07-23 21:41:01 -0700

    implemented ingredients GET

--- BackendApi/api/ingredients_lambdas/ingredients_get.py
+++ BackendApi/api/ingredients_lambdas/ingredients_get.py
@@ -15,14 +15,45 @@ from database import db_connect
     Return all ingredient data for a recipe
 """
 def lambda_handler(event, context):
+    print(f"event: {event}")
+    print(f"context: {context}")
+
     print("Get Ingredients...")
     connection = db_connect()
     cursor = connection.cursor()
     print("Connected to database")
-    print(f"event: {event}")
-    ingredients = event
+
+    if not event['pathParameters']['recipeid']:
+        return {
+            'statusCode': 400,
+            'message': json.dumps('recipeid is missing')
+        }
+    recipeId = event['pathParameters']['recipeid']
+    print(f"recipeId: {recipeId}")
+
+    # Retrieve and organize the ingredients
+    cursor.execute(f'SELECT * FROM recipes_db.ingredients WHERE recipeId = {recipeId}')
+    ingredientsSql = cursor.fetchall()
+    print(f"sql response: {ingredientsSql}")
+    if not ingredientsSql:
+        print("No content to show")
+        return {
+            'statusCode': 204
+        }
+    
+    ingredientsList = []
+    for ingredient in ingredientsSql:
+        ingredientDict = {}
+        ingredientDict['ingredientName'] = ingredient[1]
+        ingredientDict['ingredientAmount'] = ingredient[2]
+        ingredientDict['ingredientUnit'] = ingredient[3]
+        ingredientsList.append(ingredientDict)
+
+    print(f"ingredients: {ingredientsList}")
 
     return {
         'statusCode': 200,
-        'body': ingredients
+        'body': json.dumps({
+            'ingredients': ingredientsList
+        })
     }
